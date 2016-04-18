8ebc7c356063dfdfe1caad7696bccfbf5f968c2b
Justin Pease <justinpease2@gmail.com>
2016-04-18 17:38:27 -0600

    adds model test fot links

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "link" do
+    it "can create an link with valid attributes" do
+      link = Link.create(title: "This is the title if the link")
+
+      expect(link.title).to eq("This is the title if the link")
+      expect(link.read).to eq(false)
+    end
+
+    it "default read status of the link is false" do
+      link = Link.create(title: "A new Link", read: false)
+
+      expect(link.read).to eq(false)
+    end
+
+  end
+
+  describe "updates attributes" do
+    it "updates the title" do
+      link = Link.create(title: "title")
+      link.update(title: "a new title")
+
+      expect(link.title).to eq("a new title")
+    end
+
+    it "updates the title" do
+      link = Link.create(title: "title", read: false)
+      link.update(read: true)
+
+      expect(link.read).to eq(true)
+    end
+
+    it "updates the read" do
+      link = Link.create(title: "title")
+      link.update(read: false)
+
+      expect(link.read).to eq(false)
+    end
+  end
 end
